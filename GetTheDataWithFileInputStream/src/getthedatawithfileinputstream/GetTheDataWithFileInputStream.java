b2edb2e2c1a1a63146a712dbbac51aae9940bc45
selcukakarin <selcukakarin@hotmail.com>
2018-08-16 20:12:27 +0300

    Added via NetBeans

---
+++ GetTheDataWithFileInputStream/src/getthedatawithfileinputstream/GetTheDataWithFileInputStream.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package getthedatawithfileinputstream;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Selcuk
+ */
+public class GetTheDataWithFileInputStream {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       /* 
+        FileOutputStream fos=null;
+        try {
+            fos=new FileOutputStream("dosya.txt",true);
+            
+            fos.write(65);
+            fos.write(74);
+
+            //byte[] array={101,75,65,22};
+            
+            String s="Selçuk akarın";
+            byte[] sArray=s.getBytes();
+            fos.write(sArray);
+            
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found exception oluştu.");
+        } catch (IOException ex) {
+            System.out.println("Dosyaya yazılırken bir hata oluştu. ");
+        }finally{
+            
+            try {
+                fos.close();
+            } catch (IOException ex) {
+                System.out.println("Dosya kapatılırken bir hata oluştu.");
+            }
+        }
+        */
+        
+        
+        FileInputStream fis=null;
+        
+        try {
+            fis=new FileInputStream("dosya.txt");
+            
+            int deger;
+            
+            String s="";
+            
+            int say=0;
+            fis.skip(7);
+            
+            while((deger=fis.read())!=-1){
+                s+=(char)deger;
+                say++;
+                
+                if(say==4){
+                    break;
+                }
+            }
+            System.out.println("Dosyanin 7. yerinden itibaren 4 karakter : "+s);
+            
+            /*while((deger=fis.read())!=-1){
+                s+=(char)deger;
+            }
+            
+            System.out.println("Dosya içeriği : "+s);*/
+            
+            //fis.skip(8);
+            /*
+            System.out.println("Okunan karakter : "+((char)(fis.read())));
+            System.out.println("Okunan karakter : "+((char)(fis.read())));*/
+            
+           /* System.out.println("Birinci karakter : "+(char)(fis.read()));
+            System.out.println("İkinci karakter : "+(char)(fis.read()));
+            System.out.println("Üçüncü karakter : "+(char)(fis.read()));*/
+            
+            
+            
+        } catch (FileNotFoundException ex) {
+            System.out.println("Dosya bulunamadı.");
+        } catch (IOException ex) {
+            System.out.println("Dosya okunurken hata oluştu.");
+        }finally{
+            try {
+                if(fis!=null){
+                    fis.close();
+                }
+                
+            } catch (IOException ex) {
+                System.out.println("Dosya kapatılırken bir hata oluştu.");
+            }
+        }
+        
+    }
+    
+}
