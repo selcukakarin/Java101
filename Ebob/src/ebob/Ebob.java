d0d675171c66ba7139964093075e7c63786d0220
Selçuk Akarın <selcukakarin@gmail.com>
2019-10-06 14:11:32 +0300

    .
    .

--- Ebob/src/ebob/Ebob.java
+++ Ebob/src/ebob/Ebob.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 public class Ebob {
     public static int ebobBul(int a,int b){
         int ebob=1;
-        for(int i=1;i<=a&& i<=b;i++){
+        for(int i=1;i<=a && i<=b;i++){
             if((a%i==0)&&(b%i==0)){
                 ebob=i;
             }
